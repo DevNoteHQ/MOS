9c9ba65efb21d1fb0a47aff5b881e1247c2f30f0
MathiLpHD <schoepf.mathias@gmail.com>
2017-05-09 12:42:06 +0200

    Updated structure

--- Build.bat
+++ Build.bat
@@ -1,2 +1,2 @@
-bash -c "export PATH=\"$PATH:/mnt/d/OSDev/CrossCompiler/bin\" && make -C kernel"
+bash -c "export PATH=\"$PATH:/mnt/d/Coding/OSDev/CrossCompiler/bin\" && make -C kernel"
 pause
--- Multiboot.sh
+++ Multiboot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-grub-file --is-x86-multiboot /mnt/d/OSDev/MOS/kernel/bin/kernel.elf
+grub-file --is-x86-multiboot /mnt/d/Coding/OSDev/MOS/kernel/bin/kernel.elf
 if [ $? == 0 ]
 then
 echo "multiboot confirmed"
 else
 echo "the file is not multiboot"
 fi
-grub-file --is-x86-multiboot2 /mnt/d/OSDev/MOS/kernel/bin/kernel.elf
+grub-file --is-x86-multiboot2 /mnt/d/Coding/OSDev/MOS/kernel/bin/kernel.elf
 if [ $? == 0 ]
 then
 echo "multiboot2 confirmed"
