458722727d6b60029a1ece3674cd178d02fce328
MathiLpHD <schoepf.mathias@gmail.com>
2017-05-14 03:25:40 +0200

    Added header

--- system/kernel/src/init/idt.asm
+++ system/kernel/src/init/idt.asm
@@ -1,4 +1,9 @@
-
+;
+;  Copyright (c) 2011-2014 Graham Edgecombe <graham@grahamedgecombe.com>
+;
+;  Permission to use, copy, modify, and/or distribute this software for any
+;  purpose with or without fee is hereby granted, provided that the above
+;  copyright notice and this permission notice appear in all copies.
 
 [global idtr_install]
 idtr_install:
