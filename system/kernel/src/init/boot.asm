c81ad46e57c882e6ce3dec53331aa7d4887bdceb
MathiLpHD <schoepf.mathias@gmail.com>
2017-05-26 23:43:07 +0200

    Formatting is important!

--- system/kernel/src/init/boot.asm
+++ system/kernel/src/init/boot.asm
@@ -60,31 +60,30 @@ mb_hdr_end:
 align 0x1000
 
 GDT64:                           ; Global Descriptor Table (64-bit).
-    .Null: equ $ - GDT64         ; The null descriptor.
-    dw 0                         ; Limit (low).
-    dw 0                         ; Base (low).
-    db 0                         ; Base (middle)
-    db 0                         ; Access.
-    db 0                         ; Granularity.
-    db 0                         ; Base (high).
-    .Code: equ $ - GDT64         ; The code descriptor.
-    dw 0                         ; Limit (low).
-    dw 0                         ; Base (low).
-    db 0                         ; Base (middle)
-    db 10011010b                 ; Access (exec/read).
-    db 00100000b                 ; Granularity.
-    db 0                         ; Base (high).
-    .Data: equ $ - GDT64         ; The data descriptor.
-    dw 0                         ; Limit (low).
-    dw 0                         ; Base (low).
-    db 0                         ; Base (middle)
-    db 10010010b                 ; Access (read/write).
-    db 00000000b                 ; Granularity.
-    db 0                         ; Base (high).
-    .Pointer:                    ; The GDT-pointer.
-    dw $ - GDT64 - 1             ; Limit.
-    dq GDT64                     ; Base.
-
+   	.Null: equ $ - GDT64         ; The null descriptor.
+    	dw 0                         ; Limit (low).
+    	dw 0                         ; Base (low).
+    	db 0                         ; Base (middle)
+    	db 0                         ; Access.
+    	db 0                         ; Granularity.
+    	db 0                         ; Base (high).
+    	.Code: equ $ - GDT64         ; The code descriptor.
+    	dw 0                         ; Limit (low).
+    	dw 0                         ; Base (low).
+    	db 0                         ; Base (middle)
+    	db 10011010b                 ; Access (exec/read).
+    	db 00100000b                 ; Granularity.
+    	db 0                         ; Base (high).
+    	.Data: equ $ - GDT64         ; The data descriptor.
+    	dw 0                         ; Limit (low).
+    	dw 0                         ; Base (low).
+    	db 0                         ; Base (middle)
+    	db 10010010b                 ; Access (read/write).
+    	db 00000000b                 ; Granularity.
+    	db 0                         ; Base (high).
+    	.Pointer:                    ; The GDT-pointer.
+    	dw $ - GDT64 - 1             ; Limit.
+    	dq GDT64                     ; Base.
 
 [global start]
 start:
@@ -105,11 +104,11 @@ start:
 	jz .NoLongMode         ; They aren't, there is no long mode.
 
 	mov edi, 0x7E00000		; Set the destination index to 0x7E00000.
-    mov cr3, edi			; Set control register 3 to the destination index.
-    xor eax, eax			; Nullify the A-register.
-    mov ecx, 4096			; Set the C-register to 4096.
-    rep stosd				; Clear the memory.
-    mov edi, cr3			; Set the destination index to control register 3.
+    	mov cr3, edi			; Set control register 3 to the destination index.
+    	xor eax, eax			; Nullify the A-register.
+    	mov ecx, 4096			; Set the C-register to 4096.
+    	rep stosd				; Clear the memory.
+    	mov edi, cr3			; Set the destination index to control register 3.
 
 	mov DWORD [edi], 0x7E01003		; Set the uint32_t at the destination index to 0x7E01003.
 
@@ -119,7 +118,7 @@ start:
 
 	mov edi, 0x7E00FF0				; Set the destination index to 0x1000.
 	mov DWORD [edi], 0x7E01003		; Set the uint32_t at the destination index to 0x7E01003. This is for HVMA
-    add edi, 0x8					; Add 0x8 to the destination index.
+    	add edi, 0x8					; Add 0x8 to the destination index.
 	mov DWORD [edi], 0x7E00003		; Set the uint32_t at the destination index to 0x7E00003. This is for recrusive mapping
 
 	mov eax, cr4				; Set the A-register to control register 4.
