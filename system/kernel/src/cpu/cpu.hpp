dafaa8a6971a617dc64cadec0f3f2c66867e2788
DevNoteHQ <schoepf.mathias@gmail.com>
2017-11-07 00:58:59 +0100

    Nothing special

--- system/kernel/src/cpu/cpu.hpp
+++ system/kernel/src/cpu/cpu.hpp
@@ -8,30 +8,30 @@
 namespace CPU
 {
 	typedef struct{
-		uint64_t rax;
-		uint64_t rbx;
-		uint64_t rcx;
-		uint64_t rdx;
-		uint64_t rsi;
-		uint64_t rdi;
-		uint64_t rbp;
-		uint64_t r8;
-		uint64_t r9;
-		uint64_t r10;
-		uint64_t r11;
-		uint64_t r12;
-		uint64_t r13;
-		uint64_t r14;
-		uint64_t r15;
+		uint64_t iRAX;
+		uint64_t iRBX;
+		uint64_t iRCX;
+		uint64_t iRDX;
+		uint64_t iRSI;
+		uint64_t iRDI;
+		uint64_t iRBP;
+		uint64_t iR8;
+		uint64_t iR9;
+		uint64_t iR10;
+		uint64_t iR11;
+		uint64_t iR12;
+		uint64_t iR13;
+		uint64_t iR14;
+		uint64_t iR15;
 
 		uint64_t iCode;
 		uint64_t iError;
 
-		uint64_t   rip;
-		uint64_t   cs;
-		uint64_t   rflags;
-		uint64_t   rsp;
-		uint64_t   ss;
+		uint64_t   iRIP;
+		uint64_t   iCS;
+		uint64_t   iRFLAGS;
+		uint64_t   iRSP;
+		uint64_t   iSS;
 	} __attribute__((__packed__)) State;
 
 	typedef struct CPU_t {
--- system/kernel/src/interrupt/handler/fault/GeneralProtection_isr.cpp
+++ system/kernel/src/interrupt/handler/fault/GeneralProtection_isr.cpp
@@ -1,12 +1,16 @@
 
 #include <terminal/console.hpp>
 #include <interrupt/handler/stubs.hpp>
+#include <libMOS/convert/convert.hpp>
 
 namespace Interrupt::Handler
 {
 	void GeneralProtection(CPU::State *state)
 	{
-		Console::Write("\nFAULT: General Protection!");
+		Console::Write("\nFAULT: General Protection! ");
+		char cTest[64];
+		Convert::ToString(state->iError, cTest);
+		Console::Write(cTest);
 		asm volatile("hlt");
 	}
 }
