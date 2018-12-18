435c360d5f277f25b8997c1524347bb533e4977e
DevNoteHQ <schoepf.mathias@gmail.com>
2018-12-18 22:17:59 +0100

    VMM Untested 4K Alloc + Mapping

--- system/kernel/src/mm/vmm/init.cpp
+++ system/kernel/src/mm/vmm/init.cpp
@@ -32,8 +32,8 @@ namespace VMM
 		while (AllocEnd < ((uint64_t) &_end & 0x7FFFFFFFFF) - 0x200000)
 		{
 			AllocEnd = PMM::Alloc2M.Alloc();
-			//Map it
 		}
+		//Map it
 
 		PL4[PLE - 1] = (((uint64_t) &PL4[0] - HVMA) | PG_WRITABLE | PG_PRESENT);
 		PL4[PLE - 2] = (((uint64_t) &PL3[0] - HVMA) | PG_WRITABLE | PG_PRESENT);
