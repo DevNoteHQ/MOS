ac5919ddadcb5d529d18cef7dc1e57b2013e6cb1
DevNoteHQ <schoepf.mathias@gmail.com>
2019-09-06 11:10:22 +0200

    Update to GCC 7.4.0

--- Multiboot.sh
+++ Multiboot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-grub-file --is-x86-multiboot /mnt/d/Coding/OSDev/MOS/system/kernel/bin/kernel.elf
+grub-file --is-x86-multiboot "${PWD}/system/kernel/bin/kernel.elf"
 if [ $? == 0 ]
 then
 echo "Multiboot confirmed!"
 else
 echo "The file is not multiboot(1) capable"
 fi
-grub-file --is-x86-multiboot2 /mnt/d/Coding/OSDev/MOS/system/kernel/bin/kernel.elf
+grub-file --is-x86-multiboot2 "${PWD}/system/kernel/bin/kernel.elf"
 if [ $? == 0 ]
 then
 echo "Multiboot2 confirmed!"
