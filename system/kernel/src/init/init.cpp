c73850f37c79e8a753260f9a76d3138ff6103e49
DevNoteHQ <schoepf.mathias@gmail.com>
2020-03-05 18:21:58 +0100

    Test text for alexandra

--- system/kernel/src/init/init.cpp
+++ system/kernel/src/init/init.cpp
@@ -63,6 +63,8 @@ namespace System
 			uint64_t *Address = VMM::GetAddress(511, 510, 1, 0);
 			Convert::ToString((uint64_t) Timer::APIT::Ticks10ms, Test, 16);
 			Console::WriteLine(Test);
+			char Test2[] = "Hello World! This is a tutorial for Alexandra!";
+			Console::WriteLine(Test2);
 
 			while (true)
 			{
